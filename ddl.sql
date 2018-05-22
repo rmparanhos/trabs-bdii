952175985d56512c3197fe3fdebfd405c3ef097f
rmparanhos <raffaelmp@id.uff.br>
2018-05-21 23:06:10 -0300

    Esqueci de commitar esse arquivo

--- ddl.sql
+++ ddl.sql
@@ -11,11 +11,12 @@ create table restaurante(
 	constraint fk2_restaurante foreign key(categoria) references categoria(nome)
 );
 
+
 create table usuario(
 	ident_u serial,
 	nome varchar not null,
 	email varchar not null,
-	endereco varchar not null,
+	cpf varchar,
 	constraint pk_usuario primary key(ident_u)
 );
 
@@ -35,19 +36,108 @@ create table produto(
 	constraint fk1_produto foreign key(loja_id) references restaurante(ident_r)
 );
 
-create table compra(
-	produto int not null,
+create table pedido(
+	ident_pe serial,
 	comprador int not null,
-	quando timestamp not null,
+	subtotal float, --trigger
 	pagamento varchar not null,
+	numero_cartao varchar,
+	endereco varchar not null,
+	momento timestamp not null,
+	constraint pk_pedido primary key(ident_pe),
+	constraint fk1_pedido foreign key(comprador) references usuario(ident_u),
+	constraint fk2_pedido foreign key(pagamento) references tipo_pagamento(nome),
+	constraint fk3_pedido foreign key(numero_cartao) references cartao(numero),
+	constraint fk4_pedido foreign key(endereco) references endereco(rua)
+);
+
+create table compras_pedido(
+	ident_cp serial,
+	pedido int not null,
+	produto int not null,
 	pontuacao float, --trigger pra permitir apenas de 0 a 5
-	constraint pk_compra primary key(produto,comprador,quando),
-	constraint fk1_compra foreign key(produto) references produto(ident_p),
-	constraint fk2_compra foreign key(comprador) references usuario(ident_u),
-	constraint fk3_compra foreign key(pagamento) references tipo_pagamento(nome)
+	constraint pk_compras_pedido primary key(ident_cp),
+	constraint fk1_compras_pedido foreign key(pedido) references pedido(ident_pe),
+	constraint fk2_compras_pedido foreign key(produto) references produto(ident_p)
 );
 
 create table tipo_pagamento(
 	nome varchar not null,
 	constraint pk_tipo_pagamento primary key(nome)
 );
+
+create table endereco(
+	morador int not null,
+	rua varchar not null,
+	constraint pk_endereco primary key(rua),
+	constraint fk_endereco foreign key(morador) references usuario(ident_u)
+);
+
+create table cartao(
+	titular int not null,
+	numero varchar not null,
+	codigo varchar not null,
+	validade varchar not null,
+	bandeira varchar not null,
+	constraint pk_cartao primary key(numero),
+	constraint fk_cartao foreign key(titular) references usuario(ident_u)
+);
+
+CREATE TRIGGER check_pontuacao
+    BEFORE INSERT ON compras_pedido
+    FOR EACH ROW
+    EXECUTE PROCEDURE check_pontuacao();
+
+CREATE OR REPLACE FUNCTION check_pontuacao() RETURNS trigger AS $$
+BEGIN
+	if (new.pontuacao > 5 or new.pontuacao <0) then
+		raise exception 'pontuacao fora do range 0 < x < 5';
+  	end if;
+RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER calc_pontuacao_produto
+    AFTER INSERT ON compras_pedido
+    FOR EACH ROW
+    EXECUTE PROCEDURE calc_pontuacao_produto();
+
+CREATE OR REPLACE FUNCTION calc_pontuacao_produto() RETURNS trigger AS $$
+DECLARE
+ curs cursor for select pontuacao from compras_pedido where produto = new.produto;
+ soma float = 0;
+ contador int = 0;
+BEGIN
+	for record in curs loop
+		soma = soma + record.pontuacao;
+		contador = contador + 1;
+	end loop;
+	update produto
+	set pontuacao = soma/contador
+	where ident_p = new.produto;
+RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER calc_pontuacao_restaurante
+    AFTER UPDATE ON produto
+    FOR EACH ROW
+    EXECUTE PROCEDURE calc_pontuacao_restaurante();
+
+CREATE OR REPLACE FUNCTION calc_pontuacao_restaurante() RETURNS trigger AS $$
+DECLARE
+ curs cursor for select pontuacao from produto where loja_id = new.loja_id;
+ soma float = 0;
+ contador int = 0;
+BEGIN
+	for record in curs loop
+		soma = soma + record.pontuacao;
+		contador = contador + 1;
+	end loop;
+	update restaurante
+	set pontuacao = soma/contador
+	where ident_r = new.loja_id;
+RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
